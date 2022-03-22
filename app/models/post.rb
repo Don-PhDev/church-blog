f288bd6189668f46fdfb800c3d55c548cd6a5547
Don Forrest <forrest@philippinedev.com>
2022-03-23 01:30:26 +0800

    Install action_text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true, allow_blank: false
 
+  has_rich_text :body
+
   scope :by_recently_updated, -> { order(updated_at: :desc) }
 end
