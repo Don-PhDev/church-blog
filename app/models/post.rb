279bfbdb8b72a4df8c387d551c890628c3e190f8
Don Forrest <forrest@philippinedev.com>
2022-03-23 15:06:00 +0800

    Move posted_time_passed to application_record model, comment association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,8 @@
 class Post < ApplicationRecord
-  include ActionView::Helpers::DateHelper
-
   validates :title, :body, presence: true, allow_blank: false
 
   has_rich_text :body
+  has_many :comments
 
   scope :by_recently_updated, -> { order(updated_at: :desc) }
-
-  def posted_time_passed
-    if self.updated_at - self.created_at > 1
-      "Edited " + time_ago_in_words(self.updated_at) + " ago"
-    else
-      "Posted " + time_ago_in_words(self.created_at) + " ago"
-    end
-  end
 end
