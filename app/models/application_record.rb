17cc33967829ef8e024d87385440c85dafb7747d
Don Forrest <forrest@philippinedev.com>
2022-03-23 15:03:35 +0800

    Move posted_time_passed to application_record model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
+
   primary_abstract_class
+
+  def posted_time_passed
+    if self.updated_at - self.created_at > 1
+      "Edited " + time_ago_in_words(self.updated_at) + " ago"
+    else
+      "Posted " + time_ago_in_words(self.created_at) + " ago"
+    end
+  end
 end
