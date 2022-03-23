a38eb46b4b019b254025a2d573ea258357b53bad
Don Forrest <forrest@philippinedev.com>
2022-03-23 12:18:54 +0800

    Remove column body to post

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_170403) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
-    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
