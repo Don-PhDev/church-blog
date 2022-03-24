4aeca9e79afae028c1d360781f346c4e5f596662
Don Forrest <forrest@philippinedev.com>
2022-03-24 22:21:13 +0800

    Model associations and comment dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true, allow_blank: false
 
+  belongs_to :user
+
   has_rich_text :body
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
