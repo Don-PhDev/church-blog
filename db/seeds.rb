a763f18fd210e55e0d18027cdcfa825296d984a5
Don Forrest <forrest@philippinedev.com>
2022-03-26 12:32:30 +0800

    Seed Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def data_deleter
+  Comment.delete_all
+  Post.delete_all
+  User.delete_all
+end
+
+def data_creator
+  User.create(
+    email: "don@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+
+  5.times do
+    User.create!(
+      email: [
+        Faker::Internet.unique.user_name,
+        Faker::Internet.email.split("@").last
+      ].join("@"),
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  30.times do
+    Post.create!(
+      user: User.all.to_a.sample,
+      title: Faker::Book.title,
+      body: Faker::Lorem.paragraphs.join(" ")
+    )
+  end
+
+  80.times do
+    Comment.create!(
+      user: User.all.to_a.sample,
+      post: Post.all.to_a.sample,
+      body: [
+        Faker::Quote.yoda,
+        [
+          Faker::Quote.famous_last_words,
+          Faker::Quote.jack_handey
+        ].sample
+      ].join(" ")
+    )
+  end
+end
+
+def data_count_message
+  puts "Users: #{User.count}"
+  puts "Posts: #{Post.count}"
+  puts "Comments: #{Comment.count}"
+end
+
+data_deleter
+data_creator
+data_count_message
