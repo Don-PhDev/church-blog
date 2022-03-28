2fa83c592cbc0ac0aa9d893ac658409cb8de96a8
Don Forrest <forrest@philippinedev.com>
2022-03-28 18:09:05 +0800

    Add pages about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "pages/about", to: "pages#about"
+  get "pages/contact", to: "pages#contact"
+
   resources :posts do
     resources :comments
   end
