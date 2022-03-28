82d747818474b1976dfd19d828699298e716555f
Raymond Usbal <raymond@philippinedev.com>
2022-03-28 16:16:51 +0800

    Dockerized

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker-compose up -d --build
 docker exec -it churchblog_web_1 bin/bundle install
 docker exec -it churchblog_web_1 bin/rails db:create
 docker exec -it churchblog_web_1 bin/rails db:migrate
-docker exec -it churchblog_web_1 bin/rails webpacker:install
-docker exec -it churchblog_web_1 bin/rails dev:cache
+#docker exec -it churchblog_web_1 bin/rails webpacker:install
+#docker exec -it churchblog_web_1 bin/rails dev:cache
