e5790f49bd1fc6e5e453f8c6fdeb3a3907339d95
Don Forrest <forrest@philippinedev.com>
2022-03-30 18:35:53 +0800

    Customize rich text and refactor

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,18 @@ class Post < ApplicationRecord
 
   has_rich_text :body
   has_many :comments, dependent: :destroy
+
+  def first_body
+    body_text&.first.squish
+  end
+
+  def last_body
+    body_text&.last.split("]").last.squish
+  end
+
+  private
+
+  def body_text
+    self.body.to_plain_text.squish.split("[")
+  end
 end
