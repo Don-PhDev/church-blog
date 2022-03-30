8a8284cc024443d0b61c3f6eba75373af82675bf
Don Forrest <forrest@philippinedev.com>
2022-03-30 19:46:13 +0800

    Validate body and moved logic from views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true, allow_blank: false
+
   belongs_to :post
   belongs_to :user
   broadcasts_to :post
+
+  def commented_time_ago
+    posted_time_passed.downcase.split(" ").drop(1).join(" ")
+  end
 end
